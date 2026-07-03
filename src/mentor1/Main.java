832cce530497860ba8bf2246cc8d0483d6dc62a3
aleafoninser <aleafoninser@ozon.ru>
2026-07-03 23:25:48 +0300

    Работа с консолью из Main

--- src/mentor1/Main.java
+++ src/mentor1/Main.java
@@ -1,26 +1,56 @@
 package mentor1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepository();
         userRepository.create("Петя", 1);
-//        userRepository.create("Вася", 2);
-//        userRepository.create("Гоша", 3);
-//        userRepository.create("Петя", 1);
-//        userRepository.showAllUsers();
-//        userRepository.infoById(2);
-//        userRepository.deleteById(3);
+        userRepository.create("Вася", 2);
+        userRepository.create("Гоша", 3);
 
         EquipmentRepository equipmentRepository = new EquipmentRepository();
         equipmentRepository.add(new Monitor("Samsung", 1));
         equipmentRepository.add(new Mouse("Logitech", 2));
-//        equipmentRepository.add(new Computer("Dell", 3));
-//        equipmentRepository.showAll();
+        equipmentRepository.add(new Computer("Dell", 3));
 
-        equipmentRepository.assignTo(1,1);
-        equipmentRepository.assignTo(2,1);
+        ConsoleService consoleService = new ConsoleService();
 
-        equipmentRepository.showEquipmentListByUser(1);
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("\n---===Программа \"Учет техники v1.0\"===---");
+            consoleService.commandList();
+            System.out.println("Выберите пункт:");
+            String input = scanner.nextLine();
+            switch (input) {
+                case "create" -> {
+                    System.out.println("Введите имя пользователя");
+                    String name = scanner.nextLine();
+                    System.out.println("Введите id пользователя");
+                    int userId = scanner.nextInt();
+                    userRepository.create(name, userId);
+                    scanner.nextLine();
+                }
+                case "info" -> {
+                    System.out.println("Введите id пользователя");
+                    userRepository.infoById(scanner.nextInt());
+                    scanner.nextLine();
+                }
+                case "delete" -> {
+                    System.out.println("Введите id пользователя");
+                    userRepository.deleteById(scanner.nextInt());
+                    scanner.nextLine();
+                }
+                case "all" -> userRepository.showAllUsers();
+                case "exit" -> {
+                    System.out.println("Работа программы завершена");
+                    scanner.close();
+                    return;
+                }
+                default -> System.out.println("Неизвестная команда. Попробуйте еще раз");
+            }
+
+        }
     }
 }
