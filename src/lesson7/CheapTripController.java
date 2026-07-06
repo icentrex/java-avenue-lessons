04f528086039de226a1c223b68a3b346a7c40bc7
aleafoninser <aleafoninser@ozon.ru>
2026-07-07 01:16:21 +0300

    Домашнее задание 7 урок "Гид по турам"

---
+++ src/lesson7/CheapTripController.java
@@ -0,0 +1,63 @@
+package lesson7;
+
+import java.util.Scanner;
+
+public class CheapTripController {
+    private final CheapTripDB db = new CheapTripDB();
+
+    public void start() {
+        while (true) {
+            System.out.println("Добрый день! Я ваш персональный гид");
+            System.out.println("Выберите фильтр, по которому вы хотите искать туры");
+            System.out.println("1 - поиск туров по стране");
+            System.out.println("2 - поиск туров по городу");
+            System.out.println("3 - поиск туров по цене");
+            System.out.println("4 - поиск туров по цене и по звездам");
+            System.out.println("5 - вывести все туры");
+            System.out.println("0 - выйти из гида");
+
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("В какую страну вы хотите полететь?");
+                    String country = scanner.nextLine();
+                    db.searchByCountry(country);
+                }
+                case 2 -> {
+                    System.out.println("В какой город вы хотите полететь?");
+                    String city = scanner.nextLine();
+                    db.searchByCity(city);
+                }
+                case 3 -> {
+                    System.out.println("Введите бюджет на поездку (тыс. рублей)?");
+                    double price = scanner.nextDouble();
+                    db.searchByPrice(price);
+                }
+                case 4 -> {
+                    System.out.println("Введите бюджет на поездку (тыс. руб.)?");
+                    double price = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.println("Введите предпочитаемое количество звезд?");
+                    int stars = scanner.nextInt();
+                    scanner.nextLine();
+                    db.searchByPriceAndStars(price, stars);
+                }
+                case 5 -> {
+                    System.out.println("Список всех туров:");
+                    db.showTours();
+                }
+                case 0 -> {
+                    System.out.println("Гид завершил работу");
+                    return;
+                }
+                default -> System.out.println("Неверный выбор!");
+            }
+            System.out.println("\n---\nДля запуска сначала нажмите Enter...");
+            scanner.nextLine();
+
+        }
+    }
+}
---
+++ src/lesson7/CheapTripDB.java
@@ -0,0 +1,52 @@
+package lesson7;
+
+public class CheapTripDB {
+    private final Tour[] tours = {
+            new Tour("Турция", "Стамбул", "Самолет", 100_000, 5, 3, "завтрак"),
+            new Tour("Турция", "Анталья", "Самолет", 150_000, 7, 5, "завтрак"),
+            new Tour("Италия", "Рим", "Автобус", 150_000, 10, 4, "все включено"),
+            new Tour("Турция", "Самсун", "Паром", 70000, 5, 2, "завтрак + обед"),
+            new Tour("Германия", "Берлин", "Автобус", 135_000, 9, 4, "все включено"),
+            new Tour("Россия", "Алтай", "Поезд", 78000, 4, 5, "завтрак"),
+            new Tour("Иран", "Шираз", "Самолет", 80000, 5, 4, "завтрак"),
+            new Tour("Шри-ланка", "Коломбо", "Самолет", 200_000, 10, 5, "все включено")
+    };
+
+    public void searchByCountry(String country) {
+        for (Tour tour : tours) {
+            if (tour.getCountry().equalsIgnoreCase(country)) {
+                System.out.println(tour);
+            }
+        }
+    }
+
+    public void searchByCity(String city) {
+        for (Tour tour : tours) {
+            if (tour.getCity().equalsIgnoreCase(city)) {
+                System.out.println(tour);
+            }
+        }
+    }
+
+    public void searchByPrice(double price) {
+        for (Tour tour : tours) {
+            if (tour.getPrice() <= price) {
+                System.out.println(tour);
+            }
+        }
+    }
+
+    public void searchByPriceAndStars(double price, int stars) {
+        for (Tour tour : tours) {
+            if (tour.getPrice() <= price && tour.getStars() == stars) {
+                System.out.println(tour);
+            }
+        }
+    }
+
+    public void showTours() {
+        for (Tour tour : tours) {
+            System.out.println(tour);
+        }
+    }
+}
